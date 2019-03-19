d4927673c2f2b108b044272f9fc048b8978f1ba5
Derg <derg@Merlin.derg.app>
2019-03-18 23:48:07 -0700

    = Updated config enough that I may as well hose everything else

--- 06_sys_monitor/config.js
+++ 06_sys_monitor/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.maxTrackedLength = 50;  // number of samples to accumulate
-config.refreshInterval = 5000; // milliseconds
-config.machineKey = "Client";  // the key used to store your machine in the DB
+config.trackedLength = 50;    // number of samples to accumulate
+config.refreshRate = 5000;    // milliseconds
+config.machineKey = "merlin"; // the key used to store your machine in the DB
 
 module.exports = config;
