9ff964b1f116708ceb9136e12f1ef61ab6073fee
ReverieWisp <contact@reveriewisp.com>
2021-03-21 04:31:02 -0700

    Removing old embedded rat highscores api

--- 03_merlin_server/merlin_server.js
+++ 03_merlin_server/merlin_server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const util = require('util');
 const request = require('request');
-
 const fs = require('fs');
 var cors = require('cors');
 
@@ -361,62 +360,6 @@ function runMarkov(req, res) {
 }
 
 
-  ///////////////////////
- // Rat highscore API //
-///////////////////////
-
-let highscores = [
-	{
-		tag:"rat",
-		fg:"36.22.12",
-		bg:"143.97.61",
-		score:180
-	},
-	{
-		tag:"rat",
-		fg:"36.22.12",
-		bg:"143.97.61",
-		score:60
-	},
-	{
-		tag:"rat",
-		fg:"36.22.12",
-		bg:"143.97.61",
-		score:120
-	},
-	{
-		tag:"rat",
-		fg:"36.22.12",
-		bg:"143.97.61",
-		score:30
-	}
-];
-
-const api_rat = api_root + "rat/";
-app.all(api_rat, (req, res) => {
-	res.send(formatSuccess("Rat leaderboard prototype reached!"))
-});
-
-// <site>/api/v1/rat/add?tag=sampleTag&fg=someValue&bg=otherValue&score=123
-app.all(api_rat + "add", (req, res) => {
-	let query_tag = req.param("tag");
-	let query_fg = req.param("fg");
-	let query_bg = req.param("bg");
-	let query_score = req.param("score");
-
-	if(query_tag.length > 2 && query_tag != "rat")
-	{
-		query_tag = query_tag.substring(0, 2)
-	}
-
-	highscores.push({tag:query_tag, fg:query_fg, bg:query_bg, score:query_score});
-
-	res.send(formatSuccess(`Got back tag ${query_tag}, fg ${query_fg}, bg ${query_bg}, score ${query_score}`));
-});
-
-app.all(api_rat + "dump", (req, res) => {
-	res.send({entries:highscores});
-});
 
   ////////////////////
  // Tell it to run //
