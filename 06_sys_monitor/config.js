bd97646cf365ad7fceb82e1cd8504e8b94da150a
Reverie Wisp <36029079+ReverieWisp@users.noreply.github.com>
2021-03-21 02:20:46 -0700

    remove excess whitespace

--- 06_sys_monitor/config.js
+++ 06_sys_monitor/config.js
@@ -2,13 +2,13 @@ var config = {};
 // =================================
 
 // The key used to store your machine in the DB
-config.machineKey = "merlin";            
+config.machineKey = "merlin";
 
 // Number of samples to accumulate for the current machine
-config.trackedLength = 50;     
+config.trackedLength = 50;
 
 // Milliseconds between reports
-config.refreshRate = 5000;               
+config.refreshRate = 5000;
 
 // Endpoint target
 config.endpoint = "rw0.pw/api/v1/report";
