353256f97e4de2887fb040a0dcb47177ecfbe93e
Reverie Wisp <36029079+ReverieWisp@users.noreply.github.com>
2021-03-21 02:19:31 -0700

    Update config.js

--- 06_sys_monitor/config.js
+++ 06_sys_monitor/config.js
@@ -1,7 +1,17 @@
 var config = {};
+// =================================
 
-config.trackedLength = 50;    // number of samples to accumulate
-config.refreshRate = 5000;    // milliseconds
-config.machineKey = "merlin"; // the key used to store your machine in the DB
+// The key used to store your machine in the DB
+config.machineKey = "merlin";            
 
+// Number of samples to accumulate for the current machine
+config.trackedLength = 50;     
+
+// Milliseconds between reports
+config.refreshRate = 5000;               
+
+// Endpoint target
+config.endpoint = "rw0.pw/api/v1/report";
+
+// =================================
 module.exports = config;
