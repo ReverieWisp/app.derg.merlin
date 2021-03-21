f6e0b33c5fcc6fc5fb1bf20ace63e36e60a3adaa
Reverie Wisp <36029079+ReverieWisp@users.noreply.github.com>
2021-03-21 02:22:43 -0700

    Update and rename config.js to config-sample.js

--- 06_sys_monitor/config-sample.js
+++ 06_sys_monitor/config-sample.js
@@ -1,8 +1,11 @@
 var config = {};
+
+// Rename this file to config.js, and update it with your settings. 
+// Below are some sample defaults.
 // =================================
 
 // The key used to store your machine in the DB
-config.machineKey = "merlin";
+config.machineKey = "Demo";
 
 // Number of samples to accumulate for the current machine
 config.trackedLength = 50;
